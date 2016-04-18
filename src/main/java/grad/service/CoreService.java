19270e471519de06329c7114bbe47132830b25d6
JamieLv <501751443@qq.com>
2016-04-18 15:47:22 +0800

    Try Translator

--- src/main/java/grad/service/CoreService.java
+++ src/main/java/grad/service/CoreService.java
@@ -250,7 +250,7 @@ public class CoreService {
 
                         //String ISBN, String Title, String Catalog, String Author, String Translator, String Publisher, String IssueTime, String Price
                         Book book = new Book(
-                                ADD_ISBN, new_book.getTitle(), new_book.getTags(), new_book.getAuthor(), new_book.getTranslator(),
+                                ADD_ISBN, new_book.getTitle(), new_book.getTags(), new_book.getAuthor(), "Translator",
                                 new_book.getPublisher(), new_book.getPubdate(), new_book.getPrice());
                         Database.AddBook(book);
 
