d3a8114049646e7b5b5fe9418d212200afe3b5b7
JamieLv <501751443@qq.com>
2016-05-04 21:23:29 +0800

    Reminder Restriction

--- src/main/java/grad/servlet/ReminderServlet.java
+++ src/main/java/grad/servlet/ReminderServlet.java
@@ -30,13 +30,13 @@ public class ReminderServlet implements ServletContextListener {
             public void run() {
                 List<Member_Info> member_infoList = Database.getAllMember_Info();
                 for (Member_Info member_info: member_infoList) {
-                    if (Database.getBook_StatebyBorrower(member_info.getMember_ID()) != null) {
+//                    if (Database.getBook_StatebyBorrower(member_info.getMember_ID()) != null) {
                         try {
                             BorrowReminder.BorrowReminderTemplate(member_info.getMember_fromUserName());
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
-                    }
+//                    }
                 }
             }
         }, 0, 1, TimeUnit.DAYS);
