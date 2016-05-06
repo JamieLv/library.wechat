f1854b2214a85a79dd604d5e6e7d737510d0ecf5
JamieLv <501751443@qq.com>
2016-05-06 18:14:54 +0800

    user_info

--- src/main/java/grad/service/CoreService.java
+++ src/main/java/grad/service/CoreService.java
@@ -401,10 +401,13 @@ public class CoreService {
                     String Subscriber_Sex = "未知";
                     if((int)User_Info.get("sex") == 1)  {Subscriber_Sex = "男";}
                     else if ((int)User_Info.get("sex") == 2) {Subscriber_Sex = "女";}
+                    
+                    String tagid_list = Arrays.toString((int[]) User_Info.get("tagid_list"));
+
                     Subscriber_Info new_subscriber_info = new Subscriber_Info(
                             (String)User_Info.get("openid"), (String)User_Info.get("nickname"), Subscriber_Sex, (String)User_Info.get("language"),
                             (String)User_Info.get("city"), (String)User_Info.get("province"), (String)User_Info.get("country"), (String)User_Info.get("headimgurl"),
-                            (String)User_Info.get("tagid_list"));
+                            tagid_list);
                     db.Add(new_subscriber_info);
                     respContent
                             = User_Info.get("nickname") + getGreeting() + emoji(0x1F604) + "\n"
