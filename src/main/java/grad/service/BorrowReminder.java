346409d63c138321a4e47a954497313b7a1b5fc7
JamieLv <501751443@qq.com>
2016-06-06 07:29:15 +0800

    Reminder

--- src/main/java/grad/service/BorrowReminder.java
+++ src/main/java/grad/service/BorrowReminder.java
@@ -32,8 +32,8 @@ public class BorrowReminder {
 
         for (Book_State book_state: book_stateList) {
             if (Database.Borrower_RemindNeed(fromUserName) != 1){
-                Borrow_Book_Title += Borrow_Book_Title.equals("") ? book_state.getBook_Title() : "\n            " + book_state.getBook_Title();
-                Return_Book_Time += Return_Book_Time.equals("") ? book_state.getBook_Return_Time() : "\n                    " + book_state.getBook_Return_Time();
+                Borrow_Book_Title += Borrow_Book_Title.equals("") ? book_state.getBook_Title() : "\n          " + book_state.getBook_Title();
+                Return_Book_Time += Return_Book_Time.equals("") ? book_state.getBook_Return_Time() : "\n                  " + book_state.getBook_Return_Time();
             }
         }
 
